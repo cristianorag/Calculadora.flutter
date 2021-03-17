d2b57fa601d98e118a448c40bc8f2b0f3bf99366
cristianorag <cristianorag@gmail.com>
2021-03-17 11:18:32 -0300

    atualizacao do arquivo memory.dart

--- lib/components/models/memory.dart
+++ lib/components/models/memory.dart
@@ -1,8 +1,8 @@
 class Memory {
   static const operations = const ['%', '/', '*', '-', '+', '='];
   final _buffer = [0.0, 0.0];
-  final _bufferIndex = 0;
-  String operation;
+  var _bufferIndex = 0;
+  String _operation;
   String _value = '0';
   bool _wiperValue = false;
 
@@ -28,14 +28,35 @@ class Memory {
     final currentValue = wipeValue ? emptyValue  : _value;
     _value = currentValue + digit;
     _wiperValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
   _setOperation(newOperation) {
+    if(_bufferIndex == 0){
+      _operation = newOperation;
+      _bufferIndex = 1;
+    }else{
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+    }
     _wiperValue = true;
   }
 
   _allClear() {
     _value = '0';
+  }
+
+  _calculate(){
+    switch(_operation){
+      case '%' : return _buffer[0] % _buffer[1];
+      case '/' : return _buffer[0] / _buffer[1];
+      case 'x' : return _buffer[0] * _buffer[1];
+      case '-' : return _buffer[0] - _buffer[1];
+      case '+' : return _buffer[0] + _buffer[1];
+      default: return _buffer[0];
+    }
   }
 
   String get value {
